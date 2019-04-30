ef4521739a15ff13acf3e4bb6b712c796fcac435
Aidan Salzmann <aidan96@gmail.com>
2019-04-30 15:57:35 -0700

    first commit

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -86,7 +86,7 @@ public final class R {
         public static final int spread_inside = 0x7f0800e6;
         public static final int start = 0x7f0800eb;
         public static final int top = 0x7f080107;
-        public static final int wrap = 0x7f08012b;
+        public static final int wrap = 0x7f08012c;
     }
     public static final class styleable {
         private styleable() {}
