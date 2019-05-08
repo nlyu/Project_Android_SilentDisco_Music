1dffbfb0cbe1662c8efd22b5de6cb9f4909752cf
cariygan <carigan@berkeley.edu>
2019-05-08 02:53:34 -0700

    Update OpenInSpotify.java

--- app/src/main/java/com/cs160/finalproj/slientDisco/OpenInSpotify.java
+++ app/src/main/java/com/cs160/finalproj/slientDisco/OpenInSpotify.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.cs160.finalproj.slientDisco.player.Player;
 import com.cs160.finalproj.slientDisco.support.utils.UIUtils;
